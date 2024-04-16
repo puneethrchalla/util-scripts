50abaf7c5453d86f0dbcc3af70dfae10adad4e69
Puneeth Reddy Challa <41547088+puneethrchalla@users.noreply.github.com>
2024-04-16 01:36:15 -0500

    Update state.tf

--- state.tf
+++ state.tf
@@ -13,3 +13,19 @@ resource "aws_sfn_state_machine" "ecs_scale_down_sfn" {
   role_arn = aws_iam_role.sfn_role.arn
   definition = data.template_file.step_function_definition.rendered
 }
+
+
+data "aws_iam_policy_document" "sfn_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["states.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "sfn_role" {
+  name               = "ecs-scale-down-sfn-role"
+  assume_role_policy = data.aws_iam_policy_document.sfn_assume_role_policy.json
+}
